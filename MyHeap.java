2684685cbcfaabbc28cbbb7fd1e6e8bf76ce1477
cwu155 <cwu17@stuy.edu>
2019-05-01 02:39:06 -0400

    added tests

--- MyHeap.java
+++ MyHeap.java
@@ -45,11 +45,37 @@ public class MyHeap{
   }
 
   public static void main(String[] args) {
-     int[] test = {10, 3, 7, 6, 1, 4, 2, 5, 6, 22, 2, 3, 111, 44, 27};
-     //int[] test = {22, 33, 79, 4, 44};
-     //int[] test = {49,	38,	44,	25,	42,	23,	18};
-     //heapify(test);
-     heapsort(test);
-     for (int i : test){System.out.println(i);}
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          heapsort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
   }
 }
